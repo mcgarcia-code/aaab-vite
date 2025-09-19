3d4477ed9e2f3358645a851d06b712ebae227187
mcgarcia-utn <mariangarcia@frba.utn.edu.ar>
2025-09-19 11:30:16 -0300

    creacion de sitemaps y robots.txt

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import sitemap from 'vite-plugin-sitemap' // <--- 1. Importar el plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+
+    // --- 2. Añadir y configurar el plugin del sitemap ---
+    sitemap({
+      // ¡MUY IMPORTANTE! Reemplaza esto con el dominio final de tu sitio web
+      hostname: 'https://arbitroshandball.com.ar',
+
+      // Rutas generadas a partir de tu archivo de rutas de Vue
+      dynamicRoutes: [
+        '/descargas',
+        '/cursos',
+        '/designaciones',
+        '/tribunal-de-etica',
+        '/sanciones',
+      ],
+
+      // Opcional: Esto también generará un archivo robots.txt útil
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ]
+    }),
   ],
   resolve: {
     alias: {
