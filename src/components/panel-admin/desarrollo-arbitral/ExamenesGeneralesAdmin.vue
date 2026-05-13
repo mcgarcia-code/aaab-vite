bab0b6598a7a1bf5fb95420443550e9052f75a91
mcgarcia-code <garciamarianaceleste@gmail.com>
2026-05-12 21:09:26 -0300

    commit fallido observaciones admin

---
+++ src/components/panel-admin/desarrollo-arbitral/ExamenesGeneralesAdmin.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="examenes-generales-admin">
+    <h1>Exámenes Generales - Administración</h1>
+    <p>Aquí puedes gestionar los exámenes generales relacionados con el desarrollo arbitral.</p>
+    <!-- Aquí puedes agregar más contenido o funcionalidades según sea necesario -->
+  </div>
+</template>
